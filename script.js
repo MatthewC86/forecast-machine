c7bed6339c15b15810629b8bac6dde112b154a10
Matthew Courtney <cnymhw86@gmail.com>
2022-12-02 15:19:41 -0500

    added temp variables

---
+++ script.js
@@ -0,0 +1,12 @@
+var APIKey = "cf0b351b076cc017939d291aafeb2ff5"
+var city = "";
+var currentDate = "";
+var tempF = "";
+var humidityValue = "";
+var windSpeed = "";
+var weatherCondition = "";
+
+$(document).ready(function(){
+    
+    }
+  );
