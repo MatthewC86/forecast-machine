8b45b88fec99557fe50d704f650f77c1b06857e7
Matthew Courtney <cnymhw86@gmail.com>
2022-12-02 21:15:04 -0500

    added vars and city info load on click

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 var APIKey = "cf0b351b076cc017939d291aafeb2ff5"
 var city = "";
-var currentDate = "";
-var tempF = "";
+var now = dayjs();
+var currentDate = (now.format("MM/DD/YYYY"));
+var citySearch = $("#search-input");
+var searchButton = $("#searched-city");
+var temp = "";
 var humidityValue = "";
 var windSpeed = "";
 var weatherConditionIcon = "";
 var weatherConditionIconUrl = "";
 
+var url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cf0b351b076cc017939d291aafeb2ff5';
+var apiKey = '&APPID=cf0b351b076cc017939d291aafeb2ff5';
+var units = '&units=metric';
+var input;
 
-$(document).ready(function(){
-    
-    }
-  );
+//this is beginning on load city info on button press
+
+function setup() {
+    var button = select('#submit');
+        button.mousePressed(displayWeather);
+
+        input = select('#searched-city');
+}
+
